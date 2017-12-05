5b31156c193da4e4d3b191acfd3055f208b491ac
Tabish Raza <tabishraza@gmail.com>
2017-12-05 14:29:41 +0000

    returns all notes

--- spec/noteListModelTest.js
+++ spec/noteListModelTest.js
@@ -11,12 +11,14 @@ function addNoteTest(){
   assert.isInstanceOf(describe, noteList.notes[0], Note)
 }
 
-// function returnNotes(){
-//   var noteList = new NoteList();
-//   var describe = 'returns not list'
-//
-//
-// }
+function returnAllNotesTest(){
+  var noteList = new NoteList();
+  var describe = 'returns note list'
+  noteList.addNote('test')
+  console.log(noteList.allNotes().text)
+  assert.isEqual(noteList.allNotes().text === 'test')
+}
 
 NoteListModelTest();
 addNoteTest();
+returnAllNotesTest();
