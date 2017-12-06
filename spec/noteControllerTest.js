88ff219542767f0acaad64d083b84344e9533cda
Tabish Raza <tabishraza@gmail.com>
2017-12-06 15:51:47 +0000

    test for rendering html on page

---
+++ spec/noteControllerTest.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+
+  function pageContainsList(){
+    function NoteListDouble(){
+      this.notes= []
+    };
+
+    function NoteDouble(string){
+      this.text = string;
+    }
+
+    NoteListDouble.prototype = {
+      addNote: function(string){
+        var noteDouble = new NoteDouble(string);
+        this.notes.push(noteDouble)
+      },
+
+      allNotes: function(){
+        return this.notes;
+      }
+    }
+
+    var noteListDouble = new NoteListDouble();
+    noteListDouble.addNote('Marco');
+    var allNotes = noteListDouble.allNotes();
+    var noteController = new NoteController(allNotes);
+    var noteListView = new NoteListView(allNotes);
+    var render = noteListView.renderHtml();
+    noteController.printList()
+    assert.isEqual("list item produced on page", document.getElementById('list').innerHTML, "<li><div> Marco </div></li>");
+  }
+
+  pageContainsList();
+
+}
