821ffe5f907e59cdc6bacacb4c2deb6c9b533d68
Tabish Raza <tabishraza@gmail.com>
2017-12-06 10:24:09 +0000

    double of required functions

--- spec/noteListViewTest.js
+++ spec/noteListViewTest.js
@@ -1,16 +1,32 @@
 function returnNoteListView() {
-  var noteList = new NoteList();
-  var note = new Note();
-  var noteListView = new NoteListView();
 
-  noteList.addNote('Marco');
-  noteList.addNote('marco2');
-  var allNotes = noteList.allNotes()
-  console.log(allNotes + "OVER HEEERE")
+  function NoteListDouble(){
+    this.notes= []
+  };
+
+  function NoteDouble(string){
+    this.text = string;
+  }
+
+  NoteListDouble.prototype = {
+    addNote: function(string){
+      var noteDouble = new NoteDouble(string);
+      this.notes.push(noteDouble)
+    },
+
+    allNotes: function(){
+      return this.notes;
+    }
+  }
+
+  var noteListView = new NoteListView();
+  var noteListDouble = new NoteListDouble();
+  noteListDouble.addNote('Marco');
+  var allNotes = noteListDouble.allNotes();
   var renderedNotes = noteListView.renderHtml(allNotes)
   var renderedTestNote = "<li><div> Marco </div></li>"
   var describe = "The note is rendered in HTML"
-  assert.isEqual(describe, renderedNotes, renderedTestNote )
+  assert.isEqual(describe, renderedNotes, renderedTestNote)
 
 }
 returnNoteListView();
