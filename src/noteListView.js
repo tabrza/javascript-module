a70c1091090f03322a6545441fe70510db671b70
Tabish Raza <tabishraza@gmail.com>
2017-12-06 15:53:35 +0000

    taking in note list on initialize

--- src/noteListView.js
+++ src/noteListView.js
@@ -1,15 +1,18 @@
 (function(exports) {
 
-  function NoteListView(){
-
+  //noteList = NoteList
+  function NoteListView(noteList){
+    this.noteList = noteList
+    console.log("notelist", this.noteList )
   }
 
-  NoteListView.prototype.renderHtml = function(notes) {
-    var list = []
-    for(var i = 0; i < notes.length; i++){
-      list.push("<li><div> " + notes[i].text + " </div></li>")
+
+  NoteListView.prototype.renderHtml = function() {
+    var list = [];
+    for(var i = 0; i < this.noteList.length; i++){
+      list.push("<li><div> " + this.noteList[i].text + " </div></li>")
     }
-    return list.join(' ')
+    return list.join(' ');
   };
 
   exports.NoteListView = NoteListView;
