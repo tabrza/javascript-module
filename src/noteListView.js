7faad5d9f147dd315b522a9f6e7ede7f1edbc01d
Tabish Raza <tabishraza@gmail.com>
2017-12-07 10:40:38 +0000

    ul element and slice for length 20

--- src/noteListView.js
+++ src/noteListView.js
@@ -3,16 +3,15 @@
   //noteList = NoteList
   function NoteListView(noteList){
     this.noteList = noteList
-    console.log("notelist", this.noteList )
   }
 
 
   NoteListView.prototype.renderHtml = function() {
     var list = [];
     for(var i = 0; i < this.noteList.length; i++){
-      list.push("<li><div> " + this.noteList[i].text + " </div></li>")
+      list.push(`<li><div> ${this.noteList[i].text.slice(0,19)} </div></li>`)
     }
-    return list.join(' ');
+    return ("<ul>" + list.join(' ') + "</ul>");
   };
 
   exports.NoteListView = NoteListView;
